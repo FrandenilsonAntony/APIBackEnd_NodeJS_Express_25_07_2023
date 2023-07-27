a8946ddb9408c806bf625bb91803dd144dc0f254
FrandenilsonAntony <iabyantony88@gmail.com>
2023-07-27 18:13:44 -0300

    Listagem de endpoints e criação dos endpoints de leitura individual e conjunto de itens.

--- index.js
+++ index.js
@@ -7,6 +7,34 @@ app.get('/hello', (req, res) => {
   res.send('Hello World')
 });
 
+/*
+LISTA DE ENDPOINTS DA APLICAÇÃO: CRUD de mensagens
+
+-[GET] /mensagens (Retorna a lista de mensagens)
+-[GET] /mensagens/{id} (Retorna apenas uma única mensagem pelo ID)
+-[POST] /mensagens (Cria uma nova mensagem)
+-[PUT] /mensagens/{id} (Atualiza uma mensagem pelo ID)
+-[DELETE] /mensagens/{id} (Remove uma mensagem pelo ID)
+*/
+
+const mensagens = [
+    "Primeira informação",
+    "Segunda informação"
+];
+
+//-[GET] /mensagens (Retorna a lista de mensagens)
+app.get('/mensagens', (req, res) => {
+    res.send(mensagens);
+});
+
+//-[GET] /mensagens/{id} (Retorna apenas uma única mensagem pelo ID)
+app.get('/mensagens/:id', (req, res) =>{
+    const id = req.params.id - 1;
+
+    const mensagem = mensagens[id];
+    res.send(mensagem);
+});
+
 app.listen(port, () => {
 console.log(`Aplicativo rodando em http://localhost:${port}`);
 });
