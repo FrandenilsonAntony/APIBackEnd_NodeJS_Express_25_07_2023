68f19bb698e6f13763e95afb58f55bcc23f577b3
FrandenilsonAntony <iabyantony88@gmail.com>
2023-07-28 00:21:56 -0300

    BodyParser adicionado. Criação de Endpoints que cria e atualiza os dados(mensagem).

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const port = 3000;
 
+app.use(bodyParser.json());
+
 app.get('/hello', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World');
 });
 
 /*
@@ -22,17 +26,37 @@ const mensagens = [
     "Segunda informação"
 ];
 
-//-[GET] /mensagens (Retorna a lista de mensagens)
+//-[GET] /mensagens Retorna a lista de mensagens
 app.get('/mensagens', (req, res) => {
     res.send(mensagens);
 });
 
 //-[GET] /mensagens/{id} (Retorna apenas uma única mensagem pelo ID)
-app.get('/mensagens/:id', (req, res) =>{
+app.get('/mensagens/:id', (req, res) => {
     const id = req.params.id - 1;
 
     const mensagem = mensagens[id];
     res.send(mensagem);
+});
+
+//-[POST] /mensagens (Cria uma nova mensagem)
+app.post('/mensagens', (req, res) => {
+   const mensagem = req.body.mensagem;
+
+   mensagens.push(mensagem);
+
+   res.send(`Mensagem criada com sucesso: '${mensagem}'. `);
+});
+
+//-[PUT] /mensagens/{id} (Atualiza uma mensagem pelo ID)
+app.put('/mensagens/:id', (req, res) => {
+    const id = req.params.id - 1;
+    
+    const mensagem = req.body.mensagem;
+
+    mensagens[id] = mensagem;
+
+    res.send(`Mensagem atualizada com sucesso: '${mensagem}'.`);
 });
 
 app.listen(port, () => {
